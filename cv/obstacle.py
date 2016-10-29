93a77138c8f07e071c6a93958980e4d2d227d002
James <james.webber@mail.utoronto.ca>
2016-10-29 13:45:51 -0400

    cleaned up obstacle.py

--- cv/obstacle.py
+++ cv/obstacle.py
@@ -4,111 +4,33 @@ import matplotlib.pyplot as plt
 
 
 def main():
+    # full colour image loaded only so user can see output on top
     img = cv2.imread('desert.jpg')
     imgGrey = cv2.imread('desert.jpg', 0)
 
-    # imgGrey = cv2.bilateralFilter(imgGrey, 9, 30, 30)   # blur slightly to remove noise
     imgGrey = cv2.blur(imgGrey, (3, 3))
-    imgEdge = cv2.Canny(imgGrey, 150, 250)   # detect sharp edges only
+    # detect sharp edges only, so high threshold
+    imgEdge = cv2.Canny(imgGrey, 150, 250)
 
-    imWidth  = imgEdge.shape[1] - 1
-    imHeight = imgEdge.shape[0] - 1
-
+    
     # find length of edges, remove short ones
-    edge_list = get_edge_list(imgEdge, imWidth, imHeight)
-    edge_list = [a for a in edge_list if len(a) > 30]
+    edge_list = get_edge_list(imgEdge)
+    edge_list = remove_short_edges(edge_list, 30)
     edge_list = remove_circular_edges(edge_list, 20)
     
-    # flatten list and put edges back on image
+    # flatten list and make black image with edges highlighted (so user can see)
     edge_list_flat = [item for sublist in edge_list for item in sublist]
     imgEdge[:] = 0
     for point in edge_list_flat:
         imgEdge[point[1], point[0]] = 255
-    '''
-    stepSize = 8
-    edgeArray = []
 
-    for i in range (0, imWidth, stepSize):
-        for j in range(imHeight - 5, (2*imHeight)//3, -1):
-            if imgEdge.item(j, i) == 255:
-                edgeArray.append((i, j))
-                break
-            else:
-                pass
-
-    # remove edges in top 2/3 of image
-    # edgeArray = [a for a in edgeArray if a[1] > (2*imHeight)//3]
-
-    xThreshold = 8
-    yThreshold = 8
-    obstacleArray = []
-    toRemove = []
-    toSplit = []
-
-    # remove edge points that are not within the given threshold
-    # TODO: split points into separate lists if they are adjacent but
-    # one direction is greater than a threshold
-    for i, point in enumerate(edgeArray):
-        xRemoveFlag = 0
-        yRemoveFlag = 0
-
-        if i > 0:
-            # check x distance to left neighbour
-            if abs(point[0] - edgeArray[i-1][0]) > xThreshold:
-                xRemoveFlag += 1
-                # split before index
-                toSplit.append(i)
-                toRemove.append((-i, -i))
-     
-            # check y distance to left neighbour
-            if abs(point[1] - edgeArray[i-1][1]) > yThreshold:
-                yRemoveFlag += 1
-                toSplit.append(i)
-                toRemove.append((-i, -i))
-
-        if i < len(edgeArray) - 1:
-            # check x distance to right neighbour
-            if abs(point[0] - edgeArray[i+1][0]) > xThreshold:
-                xRemoveFlag += 1
-                toSplit.append(i + 1)
-                toRemove.append((-(i + 1), -(i + 1)))
-
-            # check y distance to right neighbour
-            if abs(point[1] - edgeArray[i+1][1]) > yThreshold:
-                yRemoveFlag += 1
-                toSplit.append(i + 1)
-                toRemove.append((-(i + 1), -(i + 1)))
-
-        if i == 0 or i == len(edgeArray) - 1:
-            if xRemoveFlag or yRemoveFlag:
-                toRemove.append(point)
-        else:
-            if xRemoveFlag == 2 or yRemoveFlag == 2:
-                toRemove.append(point)
-
-    for index, i in enumerate(toSplit):
-        obstacleArray.insert(index, (-i, -i))
-        
-
-    for point in toRemove:
-        obstacleArray.append(edgeArray[:edgeArray.index(point)])
-        del edgeArray[:edgeArray.index(point) + 1]
-    obstacleArray.append(edgeArray)   # add any last obstacle that may have been missed
-    obstacleArray = [a for a in obstacleArray if a != []]
-
-    # remove short edges
-    obstacleArray = [a for a in obstacleArray if len(a) > 7]
-
-    for y in range(len(obstacleArray)):
-        for x in range(len(obstacleArray[y]) - 1):
-            cv2.line(img, obstacleArray[y][x], obstacleArray[y][x + 1], (255, 0, 0), 4)
-    '''
-
+    # draw edges on original image
     for edge in edge_list:
         for i in range(len(edge) - 1):
             cv2.line(img, edge[i], edge[i + 1], (255, 0, 0), 4)
 
     # display image
+    # press any key to destroy window and view next image
     cv2.namedWindow('img', cv2.WINDOW_NORMAL)
     cv2.imshow('img', img)
     cv2.waitKey(0)
@@ -118,7 +40,9 @@ def main():
     cv2.waitKey(0)
 
 
-def get_next_edge_points(point, visited, img):
+def get_adjacent_edge_points(point, visited, img):
+    ''' Return list of points adjacent to point that have not been visited
+        and that are part of an edge (white). Diagonal adjacency is counted. '''
     points = []
 
     for i in range(-1, 2, 1):
@@ -133,9 +57,15 @@ def get_next_edge_points(point, visited, img):
 
     return points
 
-def get_edge_list(img, img_width, img_height):
+def get_edge_list(img):
+    ''' Find and return list of edges in img, where img is assumed to be
+        the output of a Canny filter. An 'edge' in edge_list is a list of points
+        that comprise the edge. '''
     edges = [[]]
     visited = []
+
+    img_width  = img.shape[1] - 1
+    img_height = img.shape[0] - 1
 
     for i in range(img_width):
         for j in range(img_height):
@@ -147,7 +77,7 @@ def get_edge_list(img, img_width, img_height):
 
                 while len(point_stack) > 0:
                     curr_point = point_stack.pop()
-                    next_points = get_next_edge_points(curr_point, visited, img)
+                    next_points = get_adjacent_edge_points(curr_point, visited, img)
 
                     for point in next_points:
                         point_stack.append(point)
@@ -162,12 +92,19 @@ def get_edge_list(img, img_width, img_height):
 
     return edges
 
+def remove_short_edges(edge_list, threshold):
+    ''' Remove edges in image that are shorter than threshold. These edges
+        are assumed to represent noise or irrelevant features such as an
+        obstacle that the rover could easily traverse. '''
+
+    return [a for a in edge_list if len(a) >= threshold]
+
 def remove_circular_edges(edge_list, threshold):
     ''' Remove edges in image that are contained entirely by a box of side
         length threshold. Short straight edges have already been removed
         so the point of this function is to remove edges with a long enough
         length to escape the first filter but that are compact in the image and
-        likely represent an irrelevant feature e.g. a pebble. '''
+        therefore likely represent an irrelevant feature e.g. a pebble. '''
 
     edges_to_remove = []
 
