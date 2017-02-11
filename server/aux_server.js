604995fbbe7172e924614f76f595aa68de325f82
Philip Huang <philip.huang@outlook.com>
2017-02-11 17:18:48 -0500

    I add the relay to aux server.

--- server/aux_server.js
+++ server/aux_server.js
@@ -3,7 +3,8 @@ var express = require('express');
 function init(model, config) {
 	model.aux = {
 		temperature: [0, 0, 0, 0, 0, 0],
-		current: [0, 0, 0, 0, 0, 0]
+		current: [0, 0, 0, 0, 0, 0],
+		relay: [false, false, false, false, false, false]
 	}
 
 	var router = express.Router();
@@ -14,6 +15,19 @@ function init(model, config) {
 		res.json(model.aux);
 	});
 
+	//get currentsensor and temperature 
+	router.put('/temperature/:temperature,:current',(req,res)=>{
+		model.aux.temperature=req.params.temperature;
+		model.aux.current=req.params.current;
+		res.json(model.aux);
+	});
+
+	//set the relay_ith element in relay to relay_state
+	router.put('/relay/:relay_i/:relay_state',(req,res)=>{ 
+		model.aux.relay[req.params.relay_i]=Boolean(req.params.relay_state);
+		res.json(model.aux);
+	});
+ 	
 	console.log('-> aux sensor started');
 	return router;
 }
