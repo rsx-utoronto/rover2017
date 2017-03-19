64ae763e11c2e56a1cc4784997f22fcc8a14c14e
c2huc2hu <c2huc2hu@gmail.com>
2017-03-18 22:31:00 -0400

    Added instructions for gps

--- server/gps_server.js
+++ server/gps_server.js
@@ -1,3 +1,16 @@
+/*
+Server for the GPS
+
+- Install the GPSLogger App for Android
+- Run the node server
+- Set the app to log to *your IP address*:8080/gps/log?lat=%LAT&lon=%LON&time=%TIME
+-> ([Hamburger button] > Logging details > Enable log to custom URL, and set the fields)
+- Start data collection
+-> You can check if the server is receiving data by running it with the --verbose option
+-> You can also check if the data is being sent by going to the Log view
+-> You may want to play with the frequency of sending data. For now, it's not that accurate
+*/
+
 var express = require('express');
 
 function init(model, config) {
