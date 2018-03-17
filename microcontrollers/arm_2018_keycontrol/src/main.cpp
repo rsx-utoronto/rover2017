1c335e5595dd02cd19dfb8fcad3d8079bcb27bd0
hschier <hudsonschier@gmail.com>
2018-03-17 14:18:54 -0400

    Implament arm demo code

--- microcontrollers/arm_2018_keycontrol/src/main.cpp
+++ microcontrollers/arm_2018_keycontrol/src/main.cpp
@@ -1,9 +1,27 @@
 #include <Arduino.h>
+#include <main.h>
 
 void setup() {
-    // put your setup code here, to run once:
+    Serial.begin(9600);
+    while (!Serial);
+    lastUpdateTime = millis();
+    pinMode(l_wrist_pin, OUTPUT);
+    pinMode(r_wrist_pin, OUTPUT);
+    pinMode(forearm_pin, OUTPUT);
+    pinMode(gripper_pin, OUTPUT);
+    pinMode(elbow_pin, OUTPUT);
+    pinMode(shoulder_pin, OUTPUT);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+    if (millis() - lastUpdateTime > 500) {
+        stopAllMotors();
+    }
+
+    if (Serial.available() > 0) {
+        uint8_t inByte = Serial.read();
+        setVariables(inByte);
+        updateMotors();
+        lastUpdateTime = millis();
+    }
 }
