9b8a60af421191c31d608085d3fcb5fcd6b0c507
Paul1234567890 <p.litvinovich@gmail.com>
2018-03-17 10:34:58 -0400

    changed output format to one value at a time in demo file

--- ArmControl/demo.py
+++ ArmControl/demo.py
@@ -2,6 +2,7 @@
 
 import pygame
 import serial
+#import sys
 
 #ser = serial.Serial('COM5', 9600)
  
@@ -60,111 +61,162 @@ done = False
 #ser.open()
 
 while not done:
+    print(1)
+    #print(sys.getsizeof(1))
+    # OUTPUT TO SERIAL
+    #ser.write(1)
 
     # --- Event Processing
-    pygame.event.get()
-    print(1)
-    keys_pressed = pygame.key.get_pressed()
-    #print( keys_pressed )
-    space = keys_pressed[32]
-    #print(space)
-    if space == 1:
-    	done = True
-    	continue
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key==pygame.K_q or event.key==pygame.K_e or event.key==pygame.K_w or event.key==pygame.K_s or event.key==pygame.K_a or event.key==pygame.K_d or event.key==pygame.K_r or event.key==pygame.K_f or event.key==pygame.K_u or event.key==pygame.K_j or event.key==pygame.K_i or event.key==pygame.K_k or event.key==pygame.K_SPACE or event.key==pygame.K_ESCAPE:
+                keys_pressed = pygame.key.get_pressed()
+                #print( keys_pressed )
+                space = keys_pressed[32]
+                #print(space)
+                #if space == 1:
+                #	done = True
+                #	continue
 
-    q = keys_pressed[113]
-    #print(q)
-    e = keys_pressed[101]
-    #print(e)
-    w = keys_pressed[119]
-    #print(w)
-    s = keys_pressed[115]
-    #print(s)
-    a = keys_pressed[97]
-    #print(a)
-    d = keys_pressed[100]
-    #print(d)
-    r = keys_pressed[114]
-    #print(r)
-    f = keys_pressed[102]
-    #print(f)
-    u = keys_pressed[117]
-    #print(u)
-    j = keys_pressed[106]
-    #print(j)
-    i = keys_pressed[105]
-    #print(i)
-    k = keys_pressed[107]
-    #print(k)
-    pressed = [q, e, w, s, a, d, r, f, u, j, i, k]
-    print( ' Q  E  W  S  A  D  R  F  U  J  I  K ' )
-    print( pressed )
+                q = keys_pressed[113]
+                #print(q)
+                e = keys_pressed[101]
+                #print(e)
+                w = keys_pressed[119]
+                #print(w)
+                s = keys_pressed[115]
+                #print(s)
+                a = keys_pressed[97]
+                #print(a)
+                d = keys_pressed[100]
+                #print(d)
+                r = keys_pressed[114]
+                #print(r)
+                f = keys_pressed[102]
+                #print(f)
+                u = keys_pressed[117]
+                #print(u)
+                j = keys_pressed[106]
+                #print(j)
+                i = keys_pressed[105]
+                #print(i)
+                k = keys_pressed[107]
+                #print(k)
+                pressed = [q, e, w, s, a, d, r, f, u, j, i, k]
+                #print( ' Q  E  W  S  A  D  R  F  U  J  I  K ' )
+                #print( pressed )
 
-    output = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    for i in range(len(output)):
-    	output[i] = list_of_key_numbers_unpressed[i] - pressed[i]
-    print('Output: ')
-    print( output )
-
-    # OUTPUT TO SERIAL
-    #ser.write(output)
-
-
-    # for event in pygame.event.get():
-    # 	print( pygame.key.get_pressed() )
-    #     #if event.type == pygame.QUIT:
-    #         #done = True
-    #         # User pressed down on a key
- 
-    #     if event.type == pygame.KEYDOWN:
-    #         #print(event.key)
-    #         # wrist rotate CCW
-    #         if event.key == pygame.K_q:
-    #         	print()
-    #             x_speed = -3
-    #         # wrist rotate CW
-    #         elif event.key == pygame.K_e:
-    #             x_speed = 3
-
-    #         # wrist pitch UP
-    #         elif event.key == pygame.K_UP:
-    #             y_speed = -3
-    #         # wrist pitch DOWN
-    #         elif event.key == pygame.K_DOWN:
-    #         	y_speed = 3
-
-    #         # forearm rotate CCW
-    #         elif event.key == pygame.K_UP:
-    #             y_speed = -3
-    #         # forearm rotate CW
-    #         elif event.key == pygame.K_DOWN:
-    #         	y_speed = 3
-
-    #         # gripper OPEN
-    #         elif event.key == pygame.K_UP:
-    #             y_speed = -3
-    #         # gripper CLOSE
-    #         elif event.key == pygame.K_DOWN:
-    #         	y_speed = 3
+                if event.key == pygame.K_q:
+                    output = 10
+                elif event.key == pygame.K_e:
+                    output = 12
+                elif event.key == pygame.K_w:
+                    output = 14
+                elif event.key == pygame.K_s:
+                    output = 16
+                elif event.key == pygame.K_a:
+                    output = 18
+                elif event.key == pygame.K_d:
+                    output = 20
+                elif event.key == pygame.K_r:
+                    output = 22
+                elif event.key == pygame.K_f:
+                    output = 24
+                elif event.key == pygame.K_u:
+                    output = 26
+                elif event.key == pygame.K_j:
+                    output = 28
+                elif event.key == pygame.K_i:
+                    output = 30
+                elif event.key == pygame.K_k:
+                    output = 32
+                elif event.key == pygame.K_SPACE:
+                    output = 2
+                elif event.key == pygame.K_ESCAPE:
+                    output = 2
+                    done = True
 
 
-    #         # QUIT key
-    #         elif event.key == pygame.K_SPACE:
-    #         	done = True
- 
-    #     # User let up on a key
-    #     elif event.type == pygame.KEYUP:
-    #         # If it is an arrow key, reset vector back to zero
-    #         if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-    #             x_speed = 0
-    #         elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-    #             y_speed = 0
- 
-    # --- Game Logic
- 
-    # Move the object according to the speed vector.
-    #x_coord = x_coord + x_speed
-    #y_coord = y_coord + y_speed
+                #output = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+                #for i in range(len(output)):
+                #	output[i] = list_of_key_numbers_unpressed[i] - pressed[i]
+                #print('Output: ')
+                print( output )
+                # OUTPUT TO SERIAL
+                #ser.write(output)
+
+        if event.type == pygame.KEYUP:
+            if event.key==pygame.K_q or event.key==pygame.K_e or event.key==pygame.K_w or event.key==pygame.K_s or event.key==pygame.K_a or event.key==pygame.K_d or event.key==pygame.K_r or event.key==pygame.K_f or event.key==pygame.K_u or event.key==pygame.K_j or event.key==pygame.K_i or event.key==pygame.K_k:
+                keys_pressed = pygame.key.get_pressed()
+                #print( keys_pressed )
+                space = keys_pressed[32]
+                #print(space)
+                #if space == 1:
+                #    done = True
+                #    continue
+
+                q = keys_pressed[113]
+                #print(q)
+                e = keys_pressed[101]
+                #print(e)
+                w = keys_pressed[119]
+                #print(w)
+                s = keys_pressed[115]
+                #print(s)
+                a = keys_pressed[97]
+                #print(a)
+                d = keys_pressed[100]
+                #print(d)
+                r = keys_pressed[114]
+                #print(r)
+                f = keys_pressed[102]
+                #print(f)
+                u = keys_pressed[117]
+                #print(u)
+                j = keys_pressed[106]
+                #print(j)
+                i = keys_pressed[105]
+                #print(i)
+                k = keys_pressed[107]
+                #print(k)
+                pressed = [q, e, w, s, a, d, r, f, u, j, i, k]
+                #print( ' Q  E  W  S  A  D  R  F  U  J  I  K ' )
+                #print( pressed )
+
+
+                if event.key == pygame.K_q:
+                    output = 11
+                elif event.key == pygame.K_e:
+                    output = 13
+                elif event.key == pygame.K_w:
+                    output = 15
+                elif event.key == pygame.K_s:
+                    output = 17
+                elif event.key == pygame.K_a:
+                    output = 19
+                elif event.key == pygame.K_d:
+                    output = 21
+                elif event.key == pygame.K_r:
+                    output = 23
+                elif event.key == pygame.K_f:
+                    output = 25
+                elif event.key == pygame.K_u:
+                    output = 27
+                elif event.key == pygame.K_j:
+                    output = 29
+                elif event.key == pygame.K_i:
+                    output = 31
+                elif event.key == pygame.K_k:
+                    output = 33
+
+                #output = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+                #for i in range(len(output)):
+                #    output[i] = list_of_key_numbers_unpressed[i] - pressed[i]
+                #print('Output: ')
+                print( output )
+                # OUTPUT TO SERIAL
+                #ser.write(output)
+
  
     # --- Drawing Code
  
