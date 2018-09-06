377e572f1bdc7d00e51c879dc85704515ce23917
Paul1234567890 <p.litvinovich@gmail.com>
2018-09-06 18:57:44 -0400

    specific to the machine build deleted

--- ArmControl/2018/build/arm_control/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cat/Desktop/RSX/rover/ArmControl/2018/src/arm_control
-# Build directory: /home/cat/Desktop/RSX/rover/ArmControl/2018/build/arm_control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
